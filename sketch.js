dd3a5c969dd1c524bc50969c77453721bf7d3a91
gamercraze1 <64150160+gamercraze1@users.noreply.github.com>
2021-05-28 00:19:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+
+var engine,world;
+var weight,speed;
+var bullet,wall, thickness; 
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,400);
+  fill(80,80,80);
+  wall=createSprite(1500,200,60,height/2);  
+  
+  bullet=createSprite(50,200,70,40);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  
+  bullet.velocityX=speed
+  
+  
 }
 
 function draw() {
+
+  
+  if(bullet.isTouching(wall)){
+    for(var x = 0;x<1;x=x+1){
+    deformation();
+    }
+    bullet.velocityX=0
+    
+  }
   background(255,255,255);  
   drawSprites();
+}
+
+function deformation(){
+if((0.5*weight*speed*speed)/(thickness*thickness*thickness)==0){
+ bullet.shapeColor=color(0,0,0)
+}else if((0.5*weight*speed*speed)/(thickness*thickness*thickness)<10&&(0.5*weight*speed*speed)/(thickness*thickness*thickness)>0){
+  bullet.shapeColor=color(0,255,0)
+}else if((0.5*weight*speed*speed)/(thickness*thickness*thickness)>10){
+  bullet.shapeColor=color(255,0,0);
+}
+
 }
